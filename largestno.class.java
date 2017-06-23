df8a25f94910c037c8ea6fcf327beb97dc1669bb
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-06-23 08:50:59 -0700

    Create largestno.class.java

---
+++ largestno.class.java
@@ -0,0 +1,21 @@
+package largestno;
+import java.util.Scanner;
+public class largestnumber{
+public static void main(String args[]){
+int x[]=new int[3];
+Scanner s=new Scanner(System.in);
+for(int i=0; i<=3;i++){
+s=x.next();
+if(1>2){
+System.out.println("result"+1);
+}
+else if(2>3){
+System.out.println("result"+2);
+}
+else
+{
+System.out.println("result"+largestno):
+}
+}
+}
+}
