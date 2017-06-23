ea0499e9738be50002892f42a6af03674f6141c1
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-06-23 08:27:22 -0700

    Create alphabet.class.java

---
+++ alphabet.class.java
@@ -0,0 +1,14 @@
+package alphabet;
+public class alphabet{
+public static void  main(String args[]){
+char chr;
+if(chr>='a' && chr<='z')
+{
+System.out.println("it is an alphabet");
+}
+else
+{
+System.out.println("it is not an alphabet");
+}
+}
+}
