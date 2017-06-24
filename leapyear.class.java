3ef7eeb56b3b54b503fda24c50e41ac8caa385c5
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-06-24 10:15:41 +0530

    Create leapyear.class.java

---
+++ leapyear.class.java
@@ -0,0 +1,16 @@
+package leapyear;
+import java.util.Scanner;
+public class leapyear{
+public static void main(String args[]){
+int n;
+n=year;
+if(n%4==0)
+{
+System.out.println("it is a leapyear");
+}
+else
+{
+System.out.println("it is not a leapyear");
+}
+}
+}
