b6d44eb2a5d81ea96ad90dd6e222ae3c10d6d3ab
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-08-12 11:03:01 +0530

    Add files via upload

---
+++ truefalse.java
@@ -0,0 +1,19 @@
+package logics;
+
+import java.util.Scanner;
+
+public class truefalse {
+public static void main(String args[]){
+	Scanner in=new Scanner(System.in);
+    System.out.println("enter the day");
+    String s1=in.nextLine();
+     	if(s1.equals("saturday")||s1.equals("sunday"))
+    	{
+    		System.out.println("false");
+    	}
+    	else
+    	{
+    		System.out.println("true");
+    	}
+    }
+}
